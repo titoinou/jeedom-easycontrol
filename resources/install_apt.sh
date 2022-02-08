050ae1a21c1c2f719f237dd196caf4863885456b
titoinou <98602478+titoinou@users.noreply.github.com>
2022-02-08 10:51:49 +0100

    Update install_apt.sh
    install if necessary

--- resources/install_apt.sh
+++ resources/install_apt.sh
@@ -1,4 +1,11 @@
 PROGRESS_FILE=/tmp/jeedom/easycontrol/dependance
+ACTUALVERSION=$(sudo bosch-xmpp -v)
+VERSION=$(sudo npm view -g bosch-xmpp version)
+if [ $ACTUALVERSION = $VERSION ]; then
+  UPDATE=0
+else
+  UPDATE=1
+fi
 touch ${PROGRESS_FILE}
 echo 0 > ${PROGRESS_FILE}
 echo "Installation dépendances EasyControl"
@@ -6,13 +13,19 @@ sudo apt-get clean
 echo 30 > ${PROGRESS_FILE}
 sudo apt-get update
 echo 50 > ${PROGRESS_FILE}
-echo "Suppressions anciennes versions de Bosch XMPP"
-sudo npm rm -g bosch-xmpp
-echo 60 > ${PROGRESS_FILE}
+if [ $UPDATE = 1 ]; then
+  echo "Suppressions anciennes versions de Bosch XMPP"
+  sudo npm remove -g bosch-xmpp
+fi
 echo 75 > ${PROGRESS_FILE}
-echo "Installation dernière version de Bosch XMPP"
-sudo npm install -g bosch-xmpp
-serverversion=`bosch-xmpp -v`;
+if [ $UPDATE = 1 ]; then
+  echo "Installation dernière version de Bosch XMPP"
+  sudo npm install -g bosch-xmpp
+fi
 echo 100 > ${PROGRESS_FILE}
-echo "Bosch XMPP version ${serverversion} installé."
+if [ $UPDATE = 1 ]; then
+  echo "Bosch XMPP version ${VERSION} installé."
+else
+  echo "Bosch XMPP version ${VERSION} est déjà installé."
+fi
 rm ${PROGRESS_FILE}
